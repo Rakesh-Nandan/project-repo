8f30eff673236b001dc968620830e1952845adcb
Rakesh Nandan <a.rakesh.nandan@accenture.com>
2020-12-11 11:25:16 +0530

    second commit

---
+++ DataFrames.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import pandas as pd
+from pandas import DataFrame 
+d=[1,2,3,4]
+d
+df=DataFrame({'Revennue':[300,400,400],'Cost':[320,500,700]})
+df
+
+
+# In[8]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
